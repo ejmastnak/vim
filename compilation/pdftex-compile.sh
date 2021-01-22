d24f5ebc710b3c25ed695d14dd70c1ecd3c123f8
Elijan Mastnak <ejmastnak@gmail.com>
2021-01-22 14:10:55 +0100

    delegate latex compilation to shell scripts

---
+++ compilation/pdftex-compile.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Arguments:
+# $1: file name without extension e.g. myfile if editing myfile.tex
+
+
+# Example group 1: ./myfile.tex:11: Extra }, or forgotten \endgroup.
+# Example group 2: l.11 \endcenter}
+err_regex="(\./${1}\.tex:[0-9]+|^l\.[0-9]+)"
+filter_regex="==> Fatal error occurred, no output PDF file produced"
+
+# This reads: pdflatex [options] myfile.tex > myfile-log.tex
+if pdflatex -file-line-error -interaction=nonstopmode -halt-on-error -shell-escape -synctex=1 "${1}.tex" > "${1}-log.txt"
+then
+  echo "Success!"
+else
+  echo "Compilation failed with errors:"
+  # Uses the err_regex string to grep the file myfile-log.txt
+  # The first grep's output is then filtered through filter_regex
+  grep -P "$err_regex" "${1}-log.txt" | grep -v "$filter_regex"
+fi
